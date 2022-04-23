ab12bc868d2ddcac8e269264fe3577ad529ff374
Stirling Hostetter <stirhoss@icloud.com>
2022-04-23 00:20:58 -0500

    Wrote test for writing to ARGV[2]

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,7 +2,18 @@
 require 'pry'
 require './lib/file_reader'
 
-handle = File.open(ARGV[0], 'r')
-text = handle.read
-handle.close
-puts text
+class NightWriter
+  attr_accessor :file_read,
+                :file_write
+
+ def initialize
+   @file_read = File.new(ARGV[0], 'r')
+   @file_write = File.new(ARGV[1], 'w')
+ end
+
+ def read
+   read_file = @file_read.read
+   @file_read.close
+   read_file
+ end
+end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,12 +1,27 @@
 # night_writer_spec.rb
+require 'rspec'
 require 'pry'
 require 'simplecov'
 require './lib/night_writer'
 
 describe 'night_writer' do
+  it 'exists' do
+    file = NightWriter.new
+
+    expect(file).to be_an_instance_of NightWriter
+  end
+
   it 'reads the first file that it takes in' do
-    file = File.open('message.txt', 'r')
+    ARGV = ['message.txt', 'braille.txt']
+    file = NightWriter.new
 
     expect(file.read).to eq "Here lies the man who's name was writ in hot-dog water.\n"
+  end
+
+  it 'writes into the second file given' do
+    ARGV = ['message.txt', 'braille.txt']
+    file = NightWriter.new
+
+    expect(file.write).to eq "HERE LIES THE MAN WHO'S NAME WAS WRIT IN HOT-DOG WATER.\n"
   end
 end
