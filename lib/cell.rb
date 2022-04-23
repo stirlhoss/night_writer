e6868d29e9e6bcdf384b733ae2536034a39ed1a0
Stirling Hostetter <stirhoss@icloud.com>
2022-04-23 17:31:11 -0500

    Cell fill functions

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,9 +2,18 @@
 require 'pry'
 
 class Cell
+  attr_reader :cell
+
   def initialize
     @cell = {1 => [nil, nil],
              2 => [nil, nil],
              3 => [nil, nil]}
   end
+
+def fill(letter)
+  cell[1] = ['O', '.']
+  cell[2] = ['.', '.']
+  cell[3] = ['.', '.']
+
+end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,4 +9,25 @@ describe Cell do
 
  expect(cell).to be_an_instance_of Cell
   end
+
+  it 'can print a cell of braille' do
+    cell = Cell.new
+
+   expect(cell.cell).to eq({
+                        1 => [nil, nil],
+                        2 => [nil, nil],
+                        3 => [nil, nil]
+                      })
+  end
+
+  it 'can fill cell to match a braille character' do
+    cell = Cell.new
+    cell.fill('A')
+
+    expect(cell.cell).to eq({
+                        1 => ['O', '.'],
+                        2 => ['.', '.'],
+                        3 => ['.', '.']
+                      })
+  end
 end
